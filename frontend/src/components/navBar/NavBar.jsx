70bc8823ac3b56c869656fadbd6d3af8f1b056f3
natnael taye <natitaye316@gmail.com>
2025-03-07 17:03:59 +0300

    input field  updated

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -9,9 +9,7 @@ import { useState } from "react";
 import "./style.css";
 import GetProfile from "../homepage/GetProfile";
 
-
 const NavBar = () => {
-
   const [searchInputIsOn, setSearchInputIsOn] = useState(false);
   const [value, setValue] = useState("");
   const [showMenu, setShowMenu] = useState(true);
@@ -21,97 +19,106 @@ const NavBar = () => {
     setSearchInputIsOn(!searchInputIsOn);
   }
 
-  function handleShowMenu(){
+  function handleShowMenu() {
     setShowMenu(!showMenu);
   }
 
- function profilePopOut(){
+  function profilePopOut() {
     setShowProfile(!showProfile);
- }
+  }
   return (
-    <nav className="flex ietms-center justify-between px-5 py-3 w-full">
-      {/* logo */}
-      <h1 className="text-2xl hidden md:flex">Mesay Furniture</h1>
-      {/* hamburger menu */}
-      <button onClick={handleShowMenu} className="flex md:hidden cursor-pointer">{showMenu ? <AiOutlineMenu/> : <LiaTimesSolid />}</button>
-      {/* Nav links */}
-      <ul className="flex items-center justify-center gap-8 text-sm uppercase hidden md:flex">
-        <li>
-          <NavLink className="hoverLink" to={"/"}>
-            home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hoverLink" to={"/shop"}>
-            shop
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hoverLink" to={"/"}>
-            pages
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hoverLink" to={"/blog"}>
-            blog
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hoverLink" to={"/about"}>
-            about
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="hoverLink" to={"/contactus"}>
-            contact us
-          </NavLink>
-        </li>
-      </ul>
-      {/* icons */}
-      <ul className="flex items-center justify-center gap-6 text-xl md:text-2xl cursor-pointer">
-        <li>
-          <div className="flex items-center ">
-            <div
-              className={
-                searchInputIsOn
-                  ? "flex items-center border-1 border-gray-200 rounded-full w-full px-3 py-1"
-                  : "hidden"
-              }
-            >
-              <input
-                type="search"
-                placeholder="Search Products"
-                onChange={(e) => setValue(e.target.value)}
-                value={value}
-                className="text-xs md:text-sm px-2 py-1 md:px-3 md:py-2 outline-none border-none rounded-full w-full"
-              />
-              <IoSearchOutline className="text-xl md:text-2xl hover:text-orange-500"/>
-            </div>
-            
-            <button
-              onClick={toggleSearchButton}
-              className="w-[37px] h-[37px] p-2 rounded-full cursor-pointer hover:text-orange-500"
-            >
-              {!searchInputIsOn ? <IoSearchOutline /> : <LiaTimesSolid />}
-            </button>
+    <>
+      <nav className="flex ietms-center justify-between px-5 py-3 w-full absolute top-0 z-20">
+        {/* logo */}
+        <h1 className="text-2xl hidden md:flex">Mesay Furniture</h1>
+        {/* hamburger menu */}
+        <button
+          onClick={handleShowMenu}
+          className="flex md:hidden cursor-pointer"
+        >
+          {showMenu ? <AiOutlineMenu /> : <LiaTimesSolid />}
+        </button>
+        {/* Nav links */}
+        <ul className="flex items-center justify-center gap-8 text-sm uppercase hidden md:flex">
+          <li>
+            <NavLink className="hoverLink" to={"/"}>
+              home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hoverLink" to={"/shop"}>
+              shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hoverLink" to={"/"}>
+              pages
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hoverLink" to={"/blog"}>
+              blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hoverLink" to={"/about"}>
+              about
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hoverLink" to={"/contactus"}>
+              contact us
+            </NavLink>
+          </li>
+        </ul>
+        {/* icons */}
+        <ul className="flex items-center justify-center gap-6 text-xl md:text-2xl cursor-pointer">
+          <li>
+            <div className="flex items-center ">
+              <div
+                className={
+                  searchInputIsOn
+                    ? "flex items-center rounded-full w-full px-3 bg-white py-1"
+                    : "hidden"
+                }
+              >
+                <input
+                  type="search"
+                  placeholder="Search Products"
+                  onChange={(e) => setValue(e.target.value)}
+                  value={value}
+                  className="text-xs md:text-sm px-2 py-1 md:px-3 md:py-2 outline-none border-none rounded-full w-full"
+                />
+                <IoSearchOutline className="text-xl md:text-2xl hover:text-orange-500" />
+              </div>
 
-          </div>
-        </li>
-       
-        <li className="hover:text-orange-500">
-          <CiHeart />
-        </li>
-        <li className="hover:text-orange-500">
-          <SlHandbag />
-        </li>
-        <li className="hover:text-orange-500" onClick={profilePopOut}>
-          <IoPersonCircleOutline />
-        </li>
-      </ul>
-      <div className={showProfile ? "hidden" : "flex"}>
-         <GetProfile />
+              <button
+                onClick={toggleSearchButton}
+                className="w-[37px] h-[37px] p-2 rounded-full cursor-pointer hover:text-orange-500"
+              >
+                {!searchInputIsOn ? <IoSearchOutline /> : <LiaTimesSolid />}
+              </button>
+            </div>
+          </li>
+
+          <li className="hover:text-orange-500">
+            <CiHeart />
+          </li>
+          <li className="hover:text-orange-500">
+            <SlHandbag />
+          </li>
+          <li className="hover:text-orange-500" onClick={profilePopOut}>
+            <IoPersonCircleOutline />
+          </li>
+        </ul>
+      </nav>
+
+      {/* drop down profile */}
+      <div className={showProfile ? "flex" : "hidden"}>
+        <GetProfile />
       </div>
-    </nav>
+
+    </>
   );
 };
 
