3a0fc00c56f95b7294ae1ea5cd14ef13e856e79d
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-15 21:43:59 +0300

    changes

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,9 +1,4 @@
 import { pool } from "../db/mysqldb.js";
-import multer from "multer";
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
-
 
 export const products = (req, res) => {
   const productQuery = "SELECT * FROM products";
@@ -21,34 +16,36 @@ export const products = (req, res) => {
 // upload the image in multer storage
 
 export const postedProducts = (req, res) => {
-    console.log("Incoming request body:", req.body);
-    console.log("Incoming file:", req.file);
-  
-    const { name, description, price } = req.body;
-  
-    try {
-      if (!req.file) {
-        return res.status(400).send("Image file is required");
-      }
-  
-      // Get only the relative path from "upload/product/filename"
-      let imgUrl = `upload/product/${req.file.filename}`;
-  
-      const insertQuery = `INSERT INTO products (description, image, name, price) VALUES (?, ?, ?, ?)`;
-      pool.query(insertQuery, [description, imgUrl, name, price], (err, results) => {
+  const { name, description, price, category} = req.body;
+  const image = req.file ? req.file.path : null;
+
+  try {
+    if (!image || !name || !description || !price || !category) {
+      return res.status(400).send("all fields are required");
+    }
+
+    // Get only the relative path from "upload/product/filename"
+    let imgUrl = `upload/product/${req.file.filename}`;
+
+    const insertQuery = `INSERT INTO products (description, image, name, price, category) VALUES (?, ?, ?, ?, ?)`;
+    pool.query(
+      insertQuery,
+      [description, imgUrl, name, price, category],
+      (err, results) => {
         if (err) {
           console.error("Error creating product:", err);
           return res.status(500).send("Server Error");
         }
         console.log("Product inserted:", results);
-  
+
         res.status(201).json({
           message: "Product created successfully",
-          product: { id: results.insertId, name, description, image: imgUrl },
+          product: { id: results.insertId, name, description, image: imgUrl, category },
         });
-      });
-    } catch (error) {
-      console.error("Server error:", error);
-      res.status(500).json("Server error");
-    }
-}
+      }
+    );
+  } catch (error) {
+    console.error("Server error:", error);
+    res.status(500).json("Server error");
+  }
+};
