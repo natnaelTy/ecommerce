7a547a4d283cf5745fff3edfbe965d9fde521406
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-18 18:16:50 +0300

    added credetial for token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// Create Axios instance
 const api = axios.create({
-    baseURL:"http://localhost:5000"
-})
+  baseURL: "http://localhost:5000",
+  withCredentials: true, // Ensure cookies are sent with requests
+});
 
-export const nPoint = " http://localhost:5000/"
+export const nPoint = "http://localhost:5000/";
 
-export default api
+export default api;
