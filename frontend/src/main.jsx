42f1d41aa7c411de750c15e8e4b9c1b75c1c594c
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-18 18:18:08 +0300

    commented out the redux-persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,17 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
 import { Toaster } from "react-hot-toast";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistStore } from "redux-persist";
 
-let persistor = persistStore(store);
+// let persistor = persistStore(store);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
+        {/* <PersistGate persistor={persistor}> */}
           <App />
-        </PersistGate>
+        {/* </PersistGate> */}
         <Toaster position="top-center" />
       </Provider>
     </BrowserRouter>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,23 +4,31 @@ import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import { combineReducers } from "@reduxjs/toolkit";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
 
-const reducer = combineReducers({
-  user: userReducer,
-});
+// const reducer = combineReducers({
+//   user: userReducer,
+// });
 
-const persistedReducer = persistReducer(persistConfig, reducer);
+// const persistedReducer = persistReducer(persistConfig, reducer);
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false, 
+//     })
+// });
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false, // Required for redux-persist
-    }),
+  reducer: {
+    user: userReducer,
+  }
+ 
 });
 
 export default store;
