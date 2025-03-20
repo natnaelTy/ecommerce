1c43f15474cc19860dc886102edd514db8d82feb
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-20 22:21:49 +0300

    new folder for productSlice

---
+++ frontend/src/store/product/productSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../services/api";
+
+// initializing states
+const initialState = {
+   product: [],
+   loading: false,
+   error: null,
+}
+// fetch products from products route end point
+export const fetchProduct = createAsyncThunk('product/fetchProduct', async (_, {rejectWithValue}) => {
+    try{
+        const response = await api.get("/products");
+        const data = await response.data.product;
+        return data;
+    }catch(error){
+      return rejectWithValue(error.message);
+    }
+})
+const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchProduct.pending, (state) => {
+           state.loading = true;
+           state.error = null;
+        })
+        .addCase(fetchProduct.fulfilled, (state, action) => {
+            state.error = null;
+            state.loading = false;
+            state.product = action.payload;
+        })
+        .addCase(fetchProduct.rejected, (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        })
+    }
+});
+
+export default productSlice.reducer;
