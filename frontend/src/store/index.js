ef95b175a936cc91a287dec8ab44d02dcc97776d
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-02 22:57:51 +0300

    removed comments

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,29 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
-import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
-import { combineReducers } from "@reduxjs/toolkit";
 import productReducer from "./product/productSlice.js";
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const reducer = combineReducers({
-//   user: userReducer,
-// });
-
-// const persistedReducer = persistReducer(persistConfig, reducer);
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false, 
-//     })
-// });
 
 const store = configureStore({
   reducer: {
