7e7131522e583b12b24ad6b00e6d1916dfa05515
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-02 22:54:06 +0300

    called the api and mapped the product data

--- frontend/src/components/homepage/NewArrival.jsx
+++ frontend/src/components/homepage/NewArrival.jsx
@@ -9,201 +9,73 @@ import { FaStar } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 export default function NewArrival() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);  
   const dispatch = useDispatch();
-  const { product, loading, error } = useSelector((state) => state.product);
+  const { productItem, loading, error } = useSelector((state) => state.product);
 
   useEffect(() => {
     dispatch(fetchProduct());
-    setProducts(product);
+    setProducts(productItem);
   }, []);
 
+ if(loading) {
+    <div>Loading..</div>
+ }
+ console.log(products);
+ 
   return (
     <>
       <div className="pb-16">
         <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">top new arrival</h2>
         <div className="flex items-center justify-center flex-col md:flex-wrap lg:flex-row gap-4">
-            <div className="newArriveContainer">
-                <div className="relative">
-                    <img src="./images/product1.jpg" alt="product 1" className="w-full"/>
-                    {/* hovering icons */}
-                    <div className="hoverBgContainer">
-                        <Link to={"/"}
-                            className="iconsWhenHovering"
-                            title="view product">
-                            <IoSearchOutline/>
-                        </Link>
-                        <Link to={'/'}
-                            className="iconsWhenHovering"
-                            title="add to wishlist">
-                            <IoIosHeartEmpty/>
-                        </Link>
-                    </div>
-                </div>
-                <div className="pt-4 pb-3 px-4">
-                    <Link to={"/"}>
-                        <h4 className="itemName">Guyer
-                            Chair</h4>
-                    </Link>
-                    <div className="flex items-baseline mb-1 space-x-2">
-                        <p className="text-sm text-primary font-semibold">45,000 birr</p>
-                        <p className="text-xs text-gray-400 line-through">55,900 birr</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="flex gap-1 text-xs text-yellow-400">
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                        </div>
-                        <div className="text-xs text-gray-500 ml-3">(150)</div>
-                    </div>
-                </div>
-                {/* add to cart button */}
-                <Link to={"/"}>
-                <button
-                    className="addToCartBtn">Add
-                    to cart</button>
-                </Link>
-            </div>
+            { products.map((_, item) => (
+                  <div key={item.id} className="newArriveContainer" >
+                  <div className="relative">
+                      <img src={item.image} alt={item.name} className="w-full"/>
+                      {/* hovering icons */}
+                      <div className="hoverBgContainer">
+                          <Link to={"/"}
+                              className="iconsWhenHovering"
+                              title="view product">
+                              <IoSearchOutline/>
+                          </Link>
+                          <Link to={'/'}
+                              className="iconsWhenHovering"
+                              title="add to wishlist">
+                              <IoIosHeartEmpty/>
+                          </Link>
+                      </div>
+                  </div>
+                  <div className="pt-4 pb-3 px-4">
+                      <Link to={"/"}>
+                          <h4 className="itemName">{item?.name}</h4>
+                      </Link>
+                      <div className="flex items-baseline mb-1 space-x-2">
+                          <p className="text-sm text-primary font-semibold">{item.price} birr</p>
+                          <p className="text-xs text-gray-400 line-through">55,900 birr</p>
+                      </div>
+                      <div className="flex items-center">
+                          <div className="flex gap-1 text-xs text-yellow-400">
+                              <FaStar/>
+                              <FaStar/>
+                              <FaStar/>
+                              <FaStar/>
+                              <FaStar/>
+                          </div>
+                          <div className="text-xs text-gray-500 ml-3">(150)</div>
+                      </div>
+                  </div>
+                  {/* add to cart button */}
+                  <Link to={"/"}>
+                  <button
+                      className="addToCartBtn">Add
+                      to cart</button>
+                  </Link>
+              </div>
+            ))}
+          
 
-            <div className="newArriveContainer">
-                <div className="relative">
-                    <img src="./images/product4.jpg" alt="product 1" className="w-full"/>
-                    <div className="newArriveContainer">
-                         <div className="hoverBgContainer">
-                        <Link to={"/"}
-                            className="iconsWhenHovering"
-                            title="view product">
-                            <IoSearchOutline/>
-                        </Link>
-                        <Link to={'/'}
-                            className="iconsWhenHovering"
-                            title="add to wishlist">
-                            <IoIosHeartEmpty/>
-                        </Link>
-                    </div>
-                    </div>
-                </div>
-                <div className="pt-4 pb-3 px-4">
-                    <Link href="#">
-                        <h4 className="itemName">Bed
-                            King Size</h4>
-                    </Link>
-                    <div className="flex items-baseline mb-1 space-x-2">
-                        <p className="text-sm text-primary font-semibold">50,000 birr</p>
-                        <p className="text-xs text-gray-400 line-through">65,900 birr</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="flex gap-1 text-xs text-yellow-400">
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                        </div>
-                        <div className="text-xs text-gray-500 ml-3">(150)</div>
-                    </div>
-                </div>
-                 {/* add to cart button */}
-                 <Link to={"/"}>
-                <button
-                    className="addToCartBtn">Add
-                    to cart</button>
-                </Link>
-            </div>
-            <div className="newArriveContainer">
-                <div className="relative">
-                    <img src="./images/product2.jpg" alt="product 1" className="w-full"/>
-                    <div className="newArriveContainer">
-                         <div className="hoverBgContainer">
-                        <Link to={"/"}
-                            className="iconsWhenHovering"
-                            title="view product">
-                            <IoSearchOutline/>
-                        </Link>
-                        <Link to={'/'}
-                            className="iconsWhenHovering"
-                            title="add to wishlist">
-                            <IoIosHeartEmpty/>
-                        </Link>
-                    </div>
-                    </div>
-                </div>
-                <div className="pt-4 pb-3 px-4">
-                    <Link href="#">
-                        <h4 className="itemName">Bed
-                            King Size</h4>
-                    </Link>
-                    <div className="flex items-baseline mb-1 space-x-2">
-                        <p className="text-sm text-primary font-semibold">50,000 birr</p>
-                        <p className="text-xs text-gray-400 line-through">65,900 birr</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="flex gap-1 text-xs text-yellow-400">
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                        </div>
-                        <div className="text-xs text-gray-500 ml-3">(150)</div>
-                    </div>
-                </div>
-                 {/* add to cart button */}
-                 <Link to={"/"}>
-                <button
-                    className="addToCartBtn">Add
-                    to cart</button>
-                </Link>
-            </div>
-            
-            <div className="newArriveContainer">
-                <div className="relative">
-                    <img src="./images/product3.jpg" alt="product 1" className="w-full"/>
-                    <div className="newArriveContainer">
-                         <div className="hoverBgContainer">
-                        <Link to={"/"}
-                            className="iconsWhenHovering"
-                            title="view product">
-                            <IoSearchOutline/>
-                        </Link>
-                        <Link to={'/'}
-                            className="iconsWhenHovering"
-                            title="add to wishlist">
-                            <IoIosHeartEmpty/>
-                        </Link>
-                    </div>
-                    </div>
-                </div>
-                <div className="pt-4 pb-3 px-4">
-                    <Link href="#">
-                        <h4 className="itemName">Bed
-                            King Size</h4>
-                    </Link>
-                    <div className="flex items-baseline mb-1 space-x-2">
-                        <p className="text-sm text-primary font-semibold">50,000 birr</p>
-                        <p className="text-xs text-gray-400 line-through">65,900 birr</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="flex gap-1 text-xs text-yellow-400">
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                        </div>
-                        <div className="text-xs text-gray-500 ml-3">(150)</div>
-                    </div>
-                </div>
-                 {/* add to cart button */}
-                 <Link to={"/"}>
-                <button
-                    className="addToCartBtn">Add
-                    to cart</button>
-                </Link>
-            </div>
+          
           
         </div>
     </div>
