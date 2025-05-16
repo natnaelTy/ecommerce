53a2d88b94f5bea6473aa884a418079f49ee475a
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-16 22:14:16 +0300

    added all homepage components in here

--- frontend/src/Layout/ClientLayout.tsx
+++ frontend/src/Layout/ClientLayout.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import NavBar from '../components/navBar/NavBar';
 import Header from '../components/navBar/Header';
 import { Outlet } from 'react-router-dom'
+import Category from "../components/homepage/Category";
+import NewArrival from "../components/homepage/NewArrival";
+import Recomended from "../components/homepage/Recomended";
+import Footer from "../components/homepage/Footer";
+
 
 function ClientLayout() {
   return (
     <>
     <Header/>
     <NavBar/>
-    <Outlet></Outlet>
+    <Outlet/>
+    <Category/>
+    <NewArrival/>
+    <Recomended/>
+    <Footer/>
     </>
   )
 }
