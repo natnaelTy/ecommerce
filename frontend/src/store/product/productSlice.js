51866f7f7f6f505a6cfe3625724a466d1e7afeaa
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-16 22:12:53 +0300

    added new api call for new arrival component

--- frontend/src/store/product/productSlice.js
+++ frontend/src/store/product/productSlice.js
@@ -1,23 +1,33 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../services/api";
 
-// initializing states
-
-// fetch products from products route end point
+// fetch all products from products route end point
 export const fetchProduct = createAsyncThunk('product/fetchProduct', async (_, {rejectWithValue}) => {
     try{
         const response = await api.get("/products");
         const data = await response.data.product;
-        console.log(data)
         return data
     }catch(error){
       return rejectWithValue(error.message);
     }
 })
+
+// fetch new arrival products
+export const fetchNewArrivalProducts = createAsyncThunk("/newarrival/fetchNewArrivalProducts", async (_, {rejectWithValue}) => {
+    try{
+      const response = await api.get("/newarrival");
+      const data = await response.data.newArrival;
+      return data;
+    }catch(error){
+       return rejectWithValue(error.message);
+    }
+})
+
 const productSlice = createSlice({
     name: "product",
     initialState: {
     productItems: [],
+    newArrivalProducts: [],
     loading: false,
     error: null,
     },
@@ -36,7 +46,22 @@ const productSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         })
+        // new arrival products
+        .addCase(fetchNewArrivalProducts.pending, (state, _) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchNewArrivalProducts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = null;
+            state.newArrivalProducts = action.payload;
+        })
+        .addCase(fetchNewArrivalProducts.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
     }
 });
+
 
 export default productSlice.reducer;
