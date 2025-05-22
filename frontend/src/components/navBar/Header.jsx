295de3bd7331dd4cd7f591520baeceec8fb2d980
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-22 23:12:46 +0300

    updated styling and added GetProfile component

--- frontend/src/components/navBar/Header.jsx
+++ frontend/src/components/navBar/Header.jsx
@@ -3,15 +3,25 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import { TiShoppingCart } from "react-icons/ti";
 import { VscAccount } from "react-icons/vsc";
 import { Link } from "react-router-dom";
+import GetProfile from "../homepage/GetProfile";
+import { useState } from "react";
+
 
 
 export default function Header() {
 
+    const [showProfile, setShowProfile] = useState(false);
+
+
+    function profilePopOut() {
+    setShowProfile(!showProfile);
+    }
+
   return (
     <header className="py-3 px-18 shadow-sm bg-white">
     <div className="flex items-center justify-between lg:justify-evenly">
-        <Link to={"/"} className="text-xl uppercase font-medium">
-          <span className="text-amber-500">Messay</span>fur.
+        <Link to={"/"} className="text-xl font-medium">
+          <span className="text-amber-500">Messay</span>Fur.
         </Link>
 
         <div className="w-full max-w-sm relative flex">
@@ -26,30 +36,28 @@ export default function Header() {
         </div>
 
         <div className="flex items-center space-x-4">
-            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+            <div className="text-center text-gray-700 hover:text-primary transition relative">
                 <div className="text-2xl">
                    <IoMdHeartEmpty/>
-                </div>
-                <div className="text-xs leading-3">Wishlist</div>
+                </div>  
                 <div
                     className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
                     8</div>
-            </a>
-            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+            </div>
+            <div className="text-center text-gray-700 hover:text-primary transition relative">
                 <div className="text-2xl">
                     <TiShoppingCart/>
                 </div>
-                <div className="text-xs leading-3">Cart</div>
                 <div
                     className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
                     2</div>
-            </a>
-            <a href="#" className="text-center text-gray-700 hover:text-primary transition relative">
+            </div>
+            <div onClick={profilePopOut} className="text-center text-gray-700 hover:text-primary transition relative">
                 <div className="text-2xl">
                     <VscAccount/>
+                    {showProfile && <GetProfile/>}
                 </div>
-                <div className="text-xs leading-3">Account</div>
-            </a>
+            </div>
         </div>
     </div>
   </header>
