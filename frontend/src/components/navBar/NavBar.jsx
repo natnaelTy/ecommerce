93058869472ca6075e368edc95c81e18b35c8ea2
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-22 23:13:40 +0300

    updated the layout

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -4,7 +4,6 @@ import { IoBedOutline } from "react-icons/io5";
 import { PiOfficeChairLight } from "react-icons/pi";
 import { useState } from "react";
 import "./style.css";
-import GetProfile from "../homepage/GetProfile";
 import { MdTableBar } from "react-icons/md";
 import { FaMattressPillow } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
@@ -14,7 +13,6 @@ const NavBar = () => {
   const [searchInputIsOn, setSearchInputIsOn] = useState(false);
   const [value, setValue] = useState("");
   const [showMenu, setShowMenu] = useState(true);
-  const [showProfile, setShowProfile] = useState(false);
 
   function toggleSearchButton() {
     setSearchInputIsOn(!searchInputIsOn);
@@ -24,60 +22,94 @@ const NavBar = () => {
     setShowMenu(!showMenu);
   }
 
-  function profilePopOut() {
-    setShowProfile(!showProfile);
-  }
   return (
     <>
       {/* navbar */}
-<nav className="bg-slate-950 flex items-center justify-around w-full">
-    <div className="flex items-center justify-around">
-        <div className="container flex items-center gap-7 relative group hidden md:flex ">
+      <nav className="bg-slate-950 flex items-center justify-around w-full h-11">
+        <div className="flex items-center justify-around">
+          <div className="container flex items-center gap-7 relative group hidden md:flex ">
             <span className="text-white flex items-center px-5 py-3 bg-amber-500 md:flex items-center cursor-pointer hidden">
-               <FiMenu/>
-               <span className="capitalize ml-2 text-white text-sm">All Categories</span>
+              <FiMenu />
+              <span className="capitalize ml-2 text-white text-sm">
+                All Categories
+              </span>
             </span>
-            
-             <div className="flex items-center space-x-6 capitalize text-sm">
-                <NavLink to={"/"} className="text-gray-200 hover:text-white transition">Home</NavLink>
-                <NavLink to={"pages/shop.html"} className="text-gray-200 hover:text-white transition">Shop</NavLink> 
-                <NavLink to={"/"} className="text-gray-200 hover:text-white transition">About us</NavLink>
-                <NavLink to={"/"} className="text-gray-200 hover:text-white transition">Contact us</NavLink>
-            </div>
-            
+
             {/* dropdown */}
-            <div
-                className="absolute z-20 w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
-                <NavLink to={"/"} className="flex  items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <RiSofaLine />
-                    <span className="ml-2 text-gray-600 text-sm">Sofa</span>
-                </NavLink>
-                <NavLink to={"/"} className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <IoBedOutline/>
-                    <span className="ml-2 text-gray-600 text-sm">Terarce</span>
-                </NavLink>
-                <NavLink to={"/"} className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <IoBedOutline/>
-                    <span className="ml-2 text-gray-600 text-sm">Bed</span>
-                </NavLink>
-                <NavLink to={"/"} className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <PiOfficeChairLight/>
-                    <span className="ml-2 text-gray-600 text-sm">Office</span>
-                </NavLink>
-                <NavLink to={"/"} className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <MdTableBar/>
-                    <span className="ml-2 text-gray-600 text-sm">Table</span>
-                </NavLink>
-                <NavLink to={"/"} className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition">
-                    <FaMattressPillow/>
-                    <span className="ml-2 text-gray-600 text-sm">Mattress</span>
-                </NavLink>
+            <div className="absolute z-20 w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+              <NavLink
+                to={"/"}
+                className="flex  items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <RiSofaLine />
+                <span className="ml-2 text-gray-600 text-sm">Sofa</span>
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <IoBedOutline />
+                <span className="ml-2 text-gray-600 text-sm">Terarce</span>
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <IoBedOutline />
+                <span className="ml-2 text-gray-600 text-sm">Bed</span>
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <PiOfficeChairLight />
+                <span className="ml-2 text-gray-600 text-sm">Office</span>
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <MdTableBar />
+                <span className="ml-2 text-gray-600 text-sm">Table</span>
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className="flex items-center px-6 py-3 hover:bg-gray-100 text-pink-600 transition"
+              >
+                <FaMattressPillow />
+                <span className="ml-2 text-gray-600 text-sm">Mattress</span>
+              </NavLink>
             </div>
+          </div>
         </div>
-    </div>
-   
-    <button href="pages/login.html" className="text-gray-200 text-sm hover:text-white transition">Login</button>
-  </nav>
+        
+        <div className="flex items-center space-x-6 capitalize text-xs md:text-sm">
+          <NavLink
+            to={"/"}
+            className="text-gray-200 hover:text-white transition"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/"}
+            className="text-gray-200 hover:text-white transition"
+          >
+            Shop
+          </NavLink>
+          <NavLink
+            to={"/"}
+            className="text-gray-200 hover:text-white transition"
+          >
+            About us
+          </NavLink>
+          <NavLink
+            to={"/"}
+            className="text-gray-200 hover:text-white transition"
+          >
+            Contact us
+          </NavLink>
+        </div>
+      </nav>
     </>
   );
 };
