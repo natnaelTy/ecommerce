c5659bf71e30f926268094e558cf15421ed6fdfc
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-22 23:06:18 +0300

    fixed component ordering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,25 @@ import ClientLayout from "./Layout/ClientLayout";
 import SignUp from "./components/pages/SignUp";
 import HomePage from "./components/homepage/HomePage";
 import LogIn from "./components/pages/LogIn";
-import Shop from "./components/shopPage/shop";
-
+import Shop from "./components/pages/shopPage/Shop";
+import Header from "./components/navBar/Header";
+import NavBar from "./components/navBar/NavBar";
+import Footer from "./components/homepage/Footer";
 
 function App() {
   return (
     <>
+    <Header/>
+    <NavBar/>
     <Routes>
        <Route path="/auth/signup" element={<SignUp />} />
        <Route path="/auth/login" element={<LogIn />} />
        <Route path="/" element={<ClientLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/shop" element={<Shop/>} />
       </Route>
+       <Route path="/shop" element={<Shop/>} />
     </Routes>
+    <Footer/>
     </>
   );
 }
