26da8bbc86478592e4b053d0c3265ba00a177721
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-05-26 17:23:46 +0300

    updated filtering features

--- frontend/src/components/pages/shopPage/SideBar.jsx
+++ frontend/src/components/pages/shopPage/SideBar.jsx
@@ -1,60 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdHome } from "react-icons/md";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { FiMenu } from "react-icons/fi";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-
-export default function SideBar({handleCheckboxChange, selectedCategory}) {
+export default function SideBar({handleCheckboxChange, selectedCategory, minPrice, maxPrice, setMaxPrice, setMinPrice}) {
 
   const { productItems } = useSelector((state) => state.product);
-  const category = (productItems.map(product => product.category));
   const [showModel, setShowModel] = useState(false);
+
+  // each category and it's length
+  const categoryLength = productItems.reduce((accumulator, product) => {
+     const category = product.category;
+     accumulator[category] = (accumulator[category] || 0) + 1;
+     return accumulator;
+  }, {});
+
+  function handleShowBox () {
+    setShowModel(!showModel);
+  }
+
 
   return (
     <> 
-    <FiMenu className="flex md:hidden"/>
-    <div className="flex flex-col items-start gap-5 mt-2 max-w-[250px] w-full p-3 border-1 border-gray-200 shadow-xs hidden md:flex">
+    <FiMenu onClick={handleShowBox} className="flex md:hidden absolute top-16 text-amber-500 left-3"/>
+    <div className={showModel ? "flex flex-col items-start gap-5 mt-2 max-w-[250px] w-full p-3 border-1 border-gray-200 rounded-md shadow-xs bg-white ml-2" : "hidden md:flex flex-col items-start gap-5 mt-2 max-w-[250px] w-full p-3 border-1 border-gray-200 rounded-md shadow-xs bg-white"}>
         <div className="flex items-center gap-2 text-base">
-            <MdHome className="text-amber-500"/>
+            <Link to={"/"}>
+             <MdHome className="text-amber-500"/>
+            </Link>
             <RiArrowRightSLine className="text-gray-500"/>
           <h1 className="text-sm">Shop</h1>
         
         </div>
         
         <div className="flex flex-col items-center items-start gap-2 w-full">
-            <h1 className="uppercase text-lg font-medium text-slate-950">category</h1>
-            <div className="flex w-full items-center justify-between ">
+            <h1 className="uppercase text-sm md:text-lg font-medium text-slate-950">category</h1>
+            <div className="flex w-full items-center justify-between text-xs md:text-sm">
             <label htmlFor="bed" className="flex gap-1 items-center" >
               <input type="checkbox" value={"bed"} checked={selectedCategory.includes("bed")} onChange={() => handleCheckboxChange('bed')}/>
               Bed
             </label>
-            <p>({category.length})</p>
+            <p>({categoryLength["bed"]})</p>
             </div>
 
-            <div className="flex w-full items-center justify-between">
+            <div className="flex w-full items-center justify-between text-xs md:text-sm">
             <label htmlFor="table" className="flex gap-1 items-center">
               <input type="checkbox" value={"table"} checked={selectedCategory.includes("table")} onChange={() => handleCheckboxChange('table')}/>
               Table
             </label>
-            <p>(15)</p>
+            <p>({categoryLength["table"]})</p>
             </div>
 
-            <div className="flex w-full items-center justify-between">
+            <div className="flex w-full items-center justify-between text-xs md:text-sm">
             <label htmlFor="sofa" className="flex gap-1 items-center">
               <input type="checkbox" value={"sofa"} checked={selectedCategory.includes("sofa")} onChange={() => handleCheckboxChange('sofa')}/>
               <p>Sofa</p>
             </label>
-            <p>(15)</p>
+            <p>({categoryLength["sofa"]})</p>
             </div>
         </div>
 
-        <div className="flex items-start gap-3 flex-col">
-             <h1 className="uppercase text-base font-medium text-slate-950">Price</h1>
+        <div className="flex items-start gap-3 flex-col text-xs md:text-sm">
+             <h1 className="uppercase text-sm md:text-lg font-medium text-slate-950">Price</h1>
              <div className="flex items-center gap-4">
-                 <input type="number" placeholder="min" className="w-20 border-1 border-gray-200 rounded-sm px-3 tex-xs"/>
+                 <input type="number" placeholder="min" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} className="w-20 border-1 border-gray-200 rounded-sm px-2"/>
                  <p>-</p>
-                 <input type="number" placeholder="max" className="w-20 border-1 border-gray-200 rounded-sm px-3 tex-xs"/>
+                 <input type="number" placeholder="max" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} className="w-20 border-1 border-gray-200 rounded-sm px-2"/>
              </div>
         </div>
     </div>
