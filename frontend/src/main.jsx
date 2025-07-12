55dddeecf00baa519be8955cc9279bf4d2440375
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-07-12 10:59:50 +0300

    fixed structure and changed toaster notification to top-right

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,19 +6,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
 import { Toaster } from "react-hot-toast";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistStore } from "redux-persist";
 
-// let persistor = persistStore(store);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        {/* <PersistGate persistor={persistor}> */}
-          <App />
-        {/* </PersistGate> */}
-        <Toaster position="top-center" />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
