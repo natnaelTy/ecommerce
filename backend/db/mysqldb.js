7e67ce95b2f56ed94480f77dc1585120a3c2e5d9
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-07-12 11:12:37 +0300

    deleted db folder cause i installed prisma

--- backend/db/mysqldb.js
+++
@@ -1,9 +0,0 @@
-  import mysql from 'mysql2';
-
-  export const pool = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "nati0975329588@babi",
-      database: "test",
-      port: 3306,
-    });
