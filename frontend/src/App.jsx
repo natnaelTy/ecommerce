6d463e6aaeb5db153d5e2b08e13d1422c5ecd57c
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-07-12 10:33:16 +0300

    fixed page route and added forgotpassword page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,32 @@
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ClientLayout from "./Layout/ClientLayout";
 import SignUp from "./components/pages/SignUp";
 import HomePage from "./components/homepage/HomePage";
 import LogIn from "./components/pages/LogIn";
 import Shop from "./components/pages/shopPage/Shop";
-import Header from "./components/navBar/Header";
-import NavBar from "./components/navBar/NavBar";
-import Footer from "./components/homepage/Footer";
+import ForgotPassword from "./components/pages/ForgotPassword";
 import ProductDetail from "./components/pages/productdetail/ProductDetail";
+import AuthRoute from "./Layout/AuthRoute";
+
 
 
 function App() {
   return (
     <>
-    <Header/>
-    <NavBar/>
-    <Routes>
-       <Route path="/auth/signup" element={<SignUp />} />
-       <Route path="/auth/login" element={<LogIn />} />
-       <Route path="/" element={<ClientLayout />}>
-        <Route index element={<HomePage />} />
-      </Route>
-       <Route path="/shop" element={<Shop/>} />
-       <Route path="/product/:id" element={<ProductDetail/>} />
-    </Routes>
-    <Footer/>
+    
+      <Routes>
+        <Route element={<ClientLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Route>
+        <Route element={<AuthRoute />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Route>
+      </Routes>
     </>
   );
 }
