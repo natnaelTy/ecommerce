01e3fca77ac9cc7d5355bd60066cd7684d3e1e7e
Natnael taye <natitaye316@gmail.com>
2025-08-04 22:06:13 +0300

    fetched user and loading state in root component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,36 @@ import Shop from "./components/pages/shopPage/Shop";
 import ForgotPassword from "./components/pages/ForgotPassword";
 import ProductDetail from "./components/pages/productdetail/ProductDetail";
 import AuthRoute from "./Layout/AuthRoute";
-
+import ResetPassword from "./components/pages/ResetPassword";
+import VerifyEmail from "./components/pages/VerifyEmail";
+import Profile from "./components/pages/account/Profile";
+import WishList from "./components/pages/account/WishList";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUser } from "./store/user/userSlice";
+import GetProfile from "./components/homepage/GetProfile";
+import { PuffLoader } from "react-spinners";
+import Cart from "./components/pages/account/Cart";
 
 
 function App() {
+
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUser())
+  }, [dispatch]);
+
+  // loading
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <PuffLoader color="#ffab00" />
+      </div>
+    );
+  }
+
   return (
     <>
     
@@ -20,11 +46,16 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/cart" element={<Cart />} />
         </Route>
         <Route element={<AuthRoute />}>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+          <Route path="/verifyemail" element={<VerifyEmail />} />
         </Route>
       </Routes>
     </>
