4d8da4527241bb916156b30a911dfc286e056dff
Natnael taye <natitaye316@gmail.com>
2025-08-27 22:06:20 +0300

    refactored and added some features

--- frontend/src/components/pages/shopPage/Shop.jsx
+++ frontend/src/components/pages/shopPage/Shop.jsx
@@ -1,5 +1,4 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchProduct } from "../../../store/product/productSlice";
 import { Link } from "react-router-dom";
@@ -9,8 +8,10 @@ import { BiSolidGrid } from "react-icons/bi";
 import { BiGridVertical } from "react-icons/bi";
 import { FaStar } from "react-icons/fa6";
 import SideBar from "./SideBar";
-import { PropagateLoader } from "react-spinners";
+import { PuffLoader } from "react-spinners";
 import { TfiMenuAlt } from "react-icons/tfi";
+import { addToCart } from "../../../store/product/productSlice";
+import { formatCurrency } from "../../../utils/formatCurrency";
 
 
 
@@ -19,11 +20,12 @@ export default function Shop() {
   const { productItems, loading, error } = useSelector(
     (state) => state.product
   );
+  const { user } = useSelector((state) => state.user);
+
   const [selectedCategory, setSelectedCategory] = useState([]);
   const [selectedSorting, setSelectedSorting] = useState("default");
   const [filteredAndSorted, setFilteredAndSorted] = useState([]);
   const [selectedLayout, setSelectedLayout] = useState(["grid-3"]);
-
 
   // fetch products from api
   useEffect(() => {
@@ -38,7 +40,7 @@ export default function Shop() {
           selectedCategory.includes(product.category)
         );
 
-  // check if checkbox is checked      
+  // check if checkbox is checked
   const handleCheckboxChange = (category) => {
     setSelectedCategory(
       (prev) =>
@@ -50,10 +52,14 @@ export default function Shop() {
 
   // layout function
   function handleLayoutChange(getCurrentLayout) {
-    setSelectedLayout((prev) => (prev.includes(getCurrentLayout) ? prev.filter((lay) => lay !== getCurrentLayout) : [getCurrentLayout]));
+    setSelectedLayout((prev) =>
+      prev.includes(getCurrentLayout)
+        ? prev.filter((lay) => lay !== getCurrentLayout)
+        : [getCurrentLayout]
+    );
   }
 
-  console.log(selectedLayout)
+  console.log(selectedLayout);
   // sorting
   useEffect(() => {
     let sorted = [...filteredProduct];
@@ -69,15 +75,14 @@ export default function Shop() {
     setFilteredAndSorted(sorted);
   }, [productItems, selectedSorting, selectedCategory]);
 
-  // loading 
+  // loading
   if (loading) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <PropagateLoader color="#ffab00" />
+        <PuffLoader color="#ffab00" />
       </div>
     );
   }
-
 
   return (
     <>
@@ -88,7 +93,7 @@ export default function Shop() {
           selectedCategory={selectedCategory}
         />
 
-      {/* shop container */}
+        {/* shop container */}
         <div className="pb-16 max-w-[1000px] w-full mx-auto text-left p-3">
           {/* layout buttons and sorting container */}
           <div className="w-full flex items-center justify-between mb-3">
@@ -103,10 +108,8 @@ export default function Shop() {
               <option value="highToLow">Sort by price: high to low</option>
             </select>
 
-          {/* layout buttons */}
-            <button
-              className="flex items-center gap-2"
-            >
+            {/* layout buttons */}
+            <button className="flex items-center gap-2">
               <BiSolidGrid
                 onClick={() => handleLayoutChange("grid-3")}
                 className={
@@ -116,7 +119,7 @@ export default function Shop() {
                 }
               />
               <BiGridVertical
-              onClick={() => handleLayoutChange("grid-2")}
+                onClick={() => handleLayoutChange("grid-2")}
                 className={
                   selectedLayout.includes("grid-2")
                     ? "bg-amber-500 text-2xl p-1 text-white rounded-xs"
@@ -124,7 +127,7 @@ export default function Shop() {
                 }
               />
               <TfiMenuAlt
-              onClick={() => handleLayoutChange("grid-1")}
+                onClick={() => handleLayoutChange("grid-1")}
                 className={
                   selectedLayout.includes("grid-1")
                     ? "bg-amber-500 text-2xl p-1 text-white rounded-xs"
@@ -137,15 +140,23 @@ export default function Shop() {
           {/* main container */}
           <div
             className={
-              selectedLayout.includes("grid-3")
-                && "grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4" ||
-                selectedLayout.includes("grid-2") && "grid grid-cols-2 gap-4" || selectedLayout.includes("grid-1") && "grid grid-cols-1 gap-6"
+              (selectedLayout.includes("grid-3") &&
+                "grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4") ||
+              (selectedLayout.includes("grid-2") && "grid grid-cols-2 gap-4") ||
+              (selectedLayout.includes("grid-1") && "grid grid-cols-1 gap-6")
             }
           >
             {/* shop card */}
             {filteredAndSorted.length > 0 ? (
               filteredAndSorted.map((item, _) => (
-                <div key={item.id} className={selectedLayout.includes("grid-1") ? "bg-white border-b-1 border-gray-300 overflow-hidden max-w-[750px] w-full flex items-center gap-6" : "newArriveContainer"}>
+                <div
+                  key={item.id}
+                  className={
+                    selectedLayout.includes("grid-1")
+                      ? "bg-white border-b-1 border-gray-300 overflow-hidden max-w-[750px] w-full flex items-center gap-6"
+                      : "newArriveContainer"
+                  }
+                >
                   {/* image container */}
                   <div className="relative max-w-[200px] md:max-w-[350px] w-full">
                     <img
@@ -162,44 +173,69 @@ export default function Shop() {
                       >
                         <IoSearchOutline />
                       </Link>
-                      <Link
-                        to={"/"}
+                      <span
+                        onClick={() => {
+                          if (!user) {
+                            // Optionally show a login prompt or toast
+                            alert("Please log in to add to wishlist.");
+                            console.error("User is not logged in");
+                            return;
+                          }
+                          dispatch(
+                            handleAddToWishlist({
+                              productId: item.id,
+                              userId: user.id,
+                            })
+                          );
+                        }}
                         className="iconsWhenHovering"
                         title="add to wishlist"
                       >
                         <IoIosHeartEmpty />
-                      </Link>
+                      </span>
                     </div>
                   </div>
-                 {/* layout for grid-cols-1 */}
-                  <div className={selectedLayout.includes("grid-1") ? " flex flex-col items-start gap-0 md:gap-4 justify-center" : "flex gap-1 flex-col"}>
-                    <div className={selectedLayout.includes("grid-1") ? "flex flex-col gap-1 md:gap-4 mb-3" : "p-1 md:p-2"}>
-                    <Link to={"/"}>
-                      <h4 className="itemName">{item.productName}</h4>
-                    </Link>
-                    <div className="flex items-baseline mb-1 space-x-2">
-                      <p className="text-sm text-primary font-semibold">
-                        {item.price} birr
-                      </p>
-                      <p className="text-xs text-gray-400 line-through">
-                        75,900 birr
-                      </p>
-                    </div>
-                    <div className="flex items-center">
-                      <div className="flex gap-1 text-xs text-yellow-400">
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                        <FaStar />
-                      </div>
-                      <div className="text-xs text-gray-500 ml-3">({item.review})</div>
-                    </div>
-                    </div>
-                      {/* add to cart button */}
-                     <Link to={"/"}>
-                          <button className="addToCartBtn">Add to cart</button>
+                  {/* layout for grid-cols-1 */}
+                  <div
+                    className={
+                      selectedLayout.includes("grid-1")
+                        ? " flex flex-col items-start gap-0 md:gap-4 justify-center"
+                        : "flex gap-1 flex-col"
+                    }
+                  >
+                    <div
+                      className={
+                        selectedLayout.includes("grid-1")
+                          ? "flex flex-col gap-1 md:gap-4 mb-3"
+                          : "p-1 md:p-2"
+                      }
+                    >
+                      <Link to={"/"}>
+                        <h4 className="itemName">{item.productName}</h4>
                       </Link>
+                      <div className="flex items-baseline mb-1 space-x-2">
+                        <p className="text-sm text-primary font-semibold">
+                          {formatCurrency(item.price, "ETB", "en-ET")}
+                        </p>
+                        <p className="text-xs text-gray-400 line-through">
+                          75,900 birr
+                        </p>
+                      </div>
+                      <div className="flex items-center">
+                        <div className="flex gap-1 text-xs text-yellow-400">
+                          <FaStar />
+                          <FaStar />
+                          <FaStar />
+                          <FaStar />
+                          <FaStar />
+                        </div>
+                        <div className="text-xs text-gray-500 ml-3">
+                          ({item.review})
+                        </div>
+                      </div>
+                    </div>
+                    {/* add to cart button */}
+                      <button onClick={() => dispatch(addToCart({productId: item.id, userId: user.id}))} className="addToCartBtn">Add to cart</button>
                   </div>
                 </div>
               ))
