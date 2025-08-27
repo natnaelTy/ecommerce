2a59b2fa11237e787b6208f9ab215db21df3d3b2
Natnael taye <natitaye316@gmail.com>
2025-08-27 21:57:35 +0300

    added some endpoints

--- backend/router/uploadRoutes.js
+++ backend/router/uploadRoutes.js
@@ -1,19 +1,66 @@
-import { postedProducts, products, newArrival } from "../controllers/products.controller.js";
+import {
+  postedProducts,
+  products,
+  newArrival,
+  addNewArrival,
+  addToWishlist,
+  addToCart,
+  getCart,
+  getWishlist,
+  removeFromCart,
+  removeFromWishlist,
+  checkout,
+  getOrdersByUser,
+  simulatePayment,
+  updateCart
+} from "../controllers/products.controller.js";
 import multer from "multer";
 import express from "express";
 
-
 const productRoute = express.Router();
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-// Get all products 
+// Get all products
 productRoute.get("/products", products);
+
+// Add product to new arrival
+productRoute.post("/newarrival", addNewArrival);
+
+// Add product to wishlist
+productRoute.post("/wishlist", addToWishlist);
+
+// Get wishlist
+productRoute.get("/wishlist/:userId", getWishlist);
+
+// Remove from wishlist
+productRoute.delete("/wishlist", removeFromWishlist);
+
+// Add to cart
+productRoute.post("/cart", addToCart);
+
+// Get user's cart
+productRoute.get("/cart/:userId", getCart);
+
+// Remove from cart
+productRoute.delete("/cart", removeFromCart);
+
+// update cart
+productRoute.patch("/cart/:userId", updateCart);
 
 // Get new arrival products
 productRoute.get("/newarrival", newArrival);
-// Product Upload 
-productRoute.post("/post-product", upload.single('image'), postedProducts);
 
+// Product Upload
+productRoute.post("/post-product", upload.single("image"), postedProducts);
+
+// checkout
+productRoute.post("/checkout", checkout);
+
+// Get orders by user
+productRoute.get("/orders/:userId", getOrdersByUser);
+
+// Simulate payment
+productRoute.post("/simulate-payment/:orderId", simulatePayment);
 
 export default productRoute;
