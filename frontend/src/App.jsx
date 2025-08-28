401c700ebef27c65a9d6c68ea54ed8c29c02db76
Natnael taye <natitaye316@gmail.com>
2025-08-28 22:18:12 +0300

    replace HomePage with Hero component and remove unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ClientLayout from "./Layout/ClientLayout";
 import SignUp from "./components/pages/SignUp";
-import HomePage from "./components/homepage/HomePage";
+import Hero from "./components/homepage/Hero";
 import LogIn from "./components/pages/LogIn";
 import Shop from "./components/pages/shopPage/Shop";
 import ForgotPassword from "./components/pages/ForgotPassword";
@@ -12,43 +12,23 @@ import ResetPassword from "./components/pages/ResetPassword";
 import VerifyEmail from "./components/pages/VerifyEmail";
 import Profile from "./components/pages/account/Profile";
 import WishList from "./components/pages/account/WishList";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchUser } from "./store/user/userSlice";
-import GetProfile from "./components/homepage/GetProfile";
-import { PuffLoader } from "react-spinners";
 import Cart from "./components/pages/account/Cart";
-
+import CheckOut from "./components/pages/account/CheckOut";
 
 function App() {
-
-  const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.user);
-
-  useEffect(() => {
-    dispatch(fetchUser())
-  }, [dispatch]);
-
-  // loading
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <PuffLoader color="#ffab00" />
-      </div>
-    );
-  }
 
   return (
     <>
     
       <Routes>
         <Route element={<ClientLayout />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<Hero />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/wishlist" element={<WishList />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<CheckOut />} />
         </Route>
         <Route element={<AuthRoute />}>
           <Route path="/signup" element={<SignUp />} />
