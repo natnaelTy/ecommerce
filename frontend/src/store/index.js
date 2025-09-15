b4e40a26db25e2ceb3fddd51e3f2dbcf6722cfc0
Natnael taye <natitaye316@gmail.com>
2025-09-15 22:07:02 +0300

    feat: add admin reducer to the Redux store configuration

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
 import productReducer from "./product/productSlice.js";
-
+import adminReducer from "./adminside/adminSlice.js";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     product: productReducer,
+    admin: adminReducer,
   }
  
 });
