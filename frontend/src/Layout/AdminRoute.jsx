7f607085f3c76e851f712fb07b4625f833545358
Natnael taye <natitaye316@gmail.com>
2025-09-17 22:00:26 +0300

    refactor: clean up AdminRoute component structure and improve readability

--- frontend/src/Layout/AdminRoute.jsx
+++ frontend/src/Layout/AdminRoute.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import SideBar from "../components/pages/adminSide//adminlayout/SideBar";
+import Navbar from "../components/pages/adminSide/adminlayout/Navbar";
 
 
 export default function AdminRoute({ children }) {
-  const { admin } = useSelector((state) => state.admin)
+  const { admin } = useSelector((state) => state.admin);
 
-  console.log("AdminRoute - admin:", admin)
-
+  const adminChildren = React.Children.toArray(children);
   if (!admin) {
-    return <Navigate to="/admin-login" />
+    return <Navigate to="/admin-login" />;
   }
 
-  return children
+  return (
+    <>
+      <div>
+        <Navbar />
+        <SideBar />
+        <main className="flex-1">
+             {adminChildren}
+        </main>
+      </div>
+    </>
+  );
 }
