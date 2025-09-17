f207f0b4c036e83a651b9901391fef1bb4a26512
Natnael taye <natitaye316@gmail.com>
2025-09-17 22:00:40 +0300

    feat: add Loading component for displaying loading state

---
+++ frontend/src/utils/loading/Loading.jsx
@@ -0,0 +1,10 @@
+import { PuffLoader } from "react-spinners";
+
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <PuffLoader color="#ffab00" />
+      </div>
+  )
+}
