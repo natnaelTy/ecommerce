bb7992acbc9e75853730081bbaf6127bdae35809
Natnael taye <natitaye316@gmail.com>
2025-09-18 22:31:58 +0300

    feat: implement add, edit, and delete product functionalities in admin controller

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -81,6 +81,31 @@ export const logoutAdmin = async (req, res) => {
   }
 };
 
+// add new product
+export const addProduct = async (req, res) => {
+  const { productName, description, price, category, image } = req.body;
+  if (!productName || !description || !price || !category || !image) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  try {
+    const newProduct = await prisma.products.create({
+      data: {
+        productName,
+        description,
+        price: Number(price),
+        category,
+        image,
+      },
+    });
+    res
+      .status(201)
+      .json({ message: "Product added successfully", product: newProduct });
+  } catch (error) {
+    console.error("Error adding product:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 // get all the products
 export const getAllProducts = async (req, res) => {
   try {
@@ -92,11 +117,75 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
+// edit product
+export const editProduct = async (req, res) => {
+  const { id } = req.params;
+  const { productName, description, price, category } = req.body;
+  const image = req.file?.fullPath;
+  try {
+    // Check if product exists
+    const existingProduct = await prisma.products.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!existingProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // update data
+    const updateData = {
+      productName:
+        productName !== undefined ? productName : existingProduct.productName,
+      description:
+        description !== undefined ? description : existingProduct.description,
+      price: price !== undefined ? Number(price) : existingProduct.price,
+      category: category !== undefined ? category : existingProduct.category,
+    };
+
+    console.log(updateData);
+    // Add image path if new image was uploaded
+    if (image) {
+      updateData.image = image;
+    }
+
+    const updatedProduct = await prisma.products.update({
+      where: { id: parseInt(id) },
+      data: updateData,
+    });
+
+    return res.status(200).json({
+      message: "Product updated successfully",
+      product: updatedProduct,
+    });
+  } catch (error) {
+    console.error("Error updating product:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// delete product
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.products.delete({
+      where: { id: Number(id) },
+    });
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 // get all the orders
 export const getAllOrders = async (req, res) => {
   try {
     const orders = await prisma.order.findMany({
-      include: { orderItems: { include: { product: true } }, payment: true, user: true },
+      include: {
+        orderItems: { include: { product: true } },
+        payment: true,
+        user: true,
+      },
     });
     res.json(orders);
   } catch (error) {
