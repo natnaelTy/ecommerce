38e341b8a68830e5d2617d6552fd39bc679eec45
Natnael taye <natitaye316@gmail.com>
2025-09-18 22:32:06 +0300

    feat: add product management routes to admin router

--- backend/router/admin-route.js
+++ backend/router/admin-route.js
@@ -1,21 +1,36 @@
 import express from "express";
-import { createAdmin, loginAdmin, getAllUsers, getAllProducts, getAllOrders, getAllPayments } from "../controllers/admin.controller.js";
+import { createAdmin, loginAdmin, getAllUsers, getAllProducts, getAllOrders, getAllPayments, addProduct, editProduct, deleteProduct } from "../controllers/admin.controller.js";
 
 
 const adminRouter = express.Router();
 
 // Admin login
 adminRouter.post("/login", loginAdmin);
+
 // Create admin
 adminRouter.post("/createAdmin", createAdmin);
+
 // Get all users
 adminRouter.get("/users", getAllUsers);
+
 // Get all products
 adminRouter.get("/products", getAllProducts);
-// Get all orders
+
+// Add a new product
+adminRouter.post("/products", addProduct);
+
+// Edit a product
+adminRouter.put("/products/edit/:id", editProduct);
+
+// Delete a product
+adminRouter.delete("/products/:id", deleteProduct);
+
+// get all the orders
 adminRouter.get("/orders", getAllOrders);
+
 // Get all payments
 adminRouter.get("/payments", getAllPayments);
+
 
 
 export default adminRouter;
