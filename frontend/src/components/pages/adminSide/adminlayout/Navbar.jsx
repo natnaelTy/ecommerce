65e56acce989605a7ba96166a0f1cc220c5993f2
Natnael taye <natitaye316@gmail.com>
2025-09-18 22:32:46 +0300

    fix: adjust Navbar styles for improved responsiveness and consistency

--- frontend/src/components/pages/adminSide/adminlayout/Navbar.jsx
+++ frontend/src/components/pages/adminSide/adminlayout/Navbar.jsx
@@ -9,11 +9,11 @@ export default function Navbar() {
 
   return (
     <div className='w-full h-16 bg-white flex items-center justify-between px-6 mx-auto sticky top-0 left-0 z-10'>
-      <h1 className="text-2xl font-semibold capitalize mx-auto">Hello, {admin.admin.fullName} 👋🏾</h1>
+      <h1 className="text-sm md:text-xl font-semibold capitalize ml-0 lg:ml-65 hidden md:block">Hello, {admin.admin.fullName} 👋🏾</h1>
       <div className='flex items-center space-x-4'>
         <Bell className="inline-block mr-6 cursor-pointer"/>
-        <img src={admin.admin.profilePhoto ? admin.admin.profilePhoto : '/images/adminlogo.png'} alt="Admin Profile" className="w-9 h-9 rounded-full object-cover"/>
-        <h1 className="capitalize font-medium">{admin.admin.fullName}</h1>
+        <img src={admin.admin.profilePhoto ? admin.admin.profilePhoto : '/images/adminlogo.png'} alt="Admin Profile" className="w-6 md:w-9 h-6 md:h-9 rounded-full object-cover"/>
+        <h1 className="capitalize font-medium text-sm md:text-base">{admin.admin.fullName}</h1>
         <ChevronDown className="inline-block  cursor-pointer"/>
       </div>
     </div>
