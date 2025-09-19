5190063681362cca42503830b10a65136b299e7c
Natnael taye <natitaye316@gmail.com>
2025-09-19 23:24:46 +0300

    refactor: streamline admin route handling and remove unused imports

--- frontend/src/Layout/AdminRoute.jsx
+++ frontend/src/Layout/AdminRoute.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { Outlet } from "react-router-dom";
 import SideBar from "../components/pages/adminSide//adminlayout/SideBar";
 import Navbar from "../components/pages/adminSide/adminlayout/Navbar";
+import { getAdminProfile } from "../store/adminside/adminSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 
 export default function AdminRoute({ children }) {
   const { admin } = useSelector((state) => state.admin);
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    dispatch(getAdminProfile());
+  }, []);
+  
   const adminChildren = React.Children.toArray(children);
-  if (!admin) {
-    return <Navigate to="/admin-login" />;
+  console.log(admin.admin.role);
+  if (!admin.admin) {
+    return navigate("/admin-login")
   }
 
   return (
