b68f56b51af2d34962fdb53ae6188a9d41a63fdb
Natnael taye <natitaye316@gmail.com>
2025-09-22 22:42:42 +0300

    feat: implement Orders component with sorting and filtering functionality

---
+++ frontend/src/components/pages/adminSide/orders/Orders.jsx
@@ -0,0 +1,128 @@
+import { fetchOrders } from "../../../../store/adminside/adminSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { formatCurrency } from "../../../../utils/formatCurrency";
+import Loading from "../../../../utils/loading/Loading";
+import toast from "react-hot-toast";
+import { Box } from "lucide-react";
+
+export default function Orders() {
+  const dispatch = useDispatch();
+  const { orders, loading, error } = useSelector((state) => state.admin);
+  const [selectedSorting, setSelectedSorting] = useState("all");
+  const [filteredAndSorted, setFilteredAndSorted] = useState([]);
+
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
+  
+  // filter orders for this week and this month
+  useEffect(() => {
+    let sorted = [...orders];
+
+    if (selectedSorting === "thisWeek") {
+      sorted = sorted.filter((order) => {
+        const orderDate = new Date(order.createdAt);
+        const weekStart = new Date();
+        weekStart.setDate(weekStart.getDate() - weekStart.getDay());
+        const weekEnd = new Date(weekStart);
+        weekEnd.setDate(weekEnd.getDate() + 6);
+        return orderDate >= weekStart && orderDate <= weekEnd;
+      });
+    } else if (selectedSorting === "thisMonth") {
+      sorted = sorted.filter((order) => {
+        const orderDate = new Date(order.createdAt);
+        const monthStart = new Date();
+        monthStart.setDate(1);
+        const monthEnd = new Date(monthStart);
+        monthEnd.setMonth(monthEnd.getMonth() + 1);
+        return orderDate >= monthStart && orderDate < monthEnd;
+      });
+    } else if (selectedSorting === "all") {
+      [...sorted];
+    }
+
+    setFilteredAndSorted(sorted.reverse());
+  }, [orders, selectedSorting]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    toast.error(error);
+  }
+
+ 
+
+  return (
+    <div className="min-h-screen max-w-[1250px] w-full px-5 ml-auto">
+      <div className="flex items-center justify-between mb-3 w-full">
+        <h2 className="text-lg md:text-xl font-semibold mb-4">
+          <Box className="inline-block bg-orange-100 text-orange-400 rounded-md p-2 size-10" />{" "}
+          {selectedSorting === "all" && "All Orders"}
+          {selectedSorting === "thisWeek" && "Orders This Week"}
+          {selectedSorting === "thisMonth" && "Orders This Month"}
+        </h2>
+        <select className="border border-gray-300 rounded-md px-4 py-2 shadow-xs" onChange={(e) => setSelectedSorting(e.target.value)} value={selectedSorting}>
+          <option value="all">All</option>
+          <option value="thisWeek">This Week</option>
+          <option value="thisMonth">This Month</option>
+        </select>
+      </div>
+      <div className="overflow-x-auto border-gray-100 border-1 shadow rounded-lg bg-white">
+        <table className="w-full">
+          <thead className="h-10 px-4">
+            <tr className="text-left">
+              <th className="px-4 py-2">Product</th>
+              <th className="px-4 py-2">Order ID</th>
+              <th className="px-4 py-2">Customer Name</th>
+              <th className="px-4 py-2">Amount</th>
+              <th className="px-4 py-2">Status</th>
+              <th className="px-4 py-2">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAndSorted.length > 0 ? (
+              filteredAndSorted.map((order) => (
+                <tr key={order.id} className="text-left capitalize font-medium">
+                  <td className="px-4 py-2">
+                    <img
+                      src={order.orderItems?.[0]?.product?.image}
+                      alt={order.orderItems?.[0]?.product?.productName}
+                      className="w-8 h-8 object-cover rounded-sm"
+                    />
+                  </td>
+                  <td className="px-4 py-2">#{order.id}</td>
+                  <td className="px-4 py-2">{order.user.fullName}</td>
+                  <td className="px-4 py-2">
+                    {formatCurrency(order.total, "ETB", "en-ET")}
+                  </td>
+                  <td
+                    className={`px-4 rounded-2xl ${
+                      order.status === "PAID"
+                        ? "text-green-500 bg-green-100 "
+                        : "text-red-500 "
+                    }`}
+                  >
+                    {order.status}
+                  </td>
+                  <td className="px-4 py-2">
+                    {new Date(order.createdAt).toLocaleDateString()}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6" className="px-4 py-2 text-gray-500">
+                  No orders found
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
