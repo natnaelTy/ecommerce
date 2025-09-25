a840b6c2bab4146ac1388632b7c282f0821fa232
Natnael taye <natitaye316@gmail.com>
2025-09-25 23:01:50 +0300

    fix: remove Bell icon from Navbar component

--- frontend/src/components/pages/adminSide/adminlayout/Navbar.jsx
+++ frontend/src/components/pages/adminSide/adminlayout/Navbar.jsx
@@ -26,7 +26,6 @@ export default function Navbar() {
     <div className='w-full h-16 bg-white flex items-center justify-between px-6 mx-auto sticky top-0 left-0 z-10 shadow'>
       <h1 className="text-sm md:text-xl font-semibold capitalize ml-0 lg:ml-65 hidden md:block">Hello, {admin?.fullName} 👋🏾</h1>
       <div className='flex items-center space-x-4'>
-        <Bell className="inline-block mr-6 cursor-pointer"/>
         <img src={admin?.profilePhoto ? admin?.profilePhoto : '/images/adminlogo.png'} alt="Admin Profile" className="w-6 md:w-9 h-6 md:h-9 rounded-full object-cover"/>
         <h1 className="capitalize font-medium text-sm md:text-base">{admin?.fullName}</h1>
         <span onClick={() => setIsModalOpen(!isModalOpen)} className="inline-block  cursor-pointer">
