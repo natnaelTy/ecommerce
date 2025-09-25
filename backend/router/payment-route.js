1c4951e82dce5d9f2ce61598c3e2a556d6b60867
Natnael taye <natitaye316@gmail.com>
2025-09-25 22:59:29 +0300

    fix: update payment routes for clarity and consistency

--- backend/router/payment-route.js
+++ backend/router/payment-route.js
@@ -8,7 +8,9 @@ paymentRouter.post("/initialize", InitializePayment);
 // verify payment
 paymentRouter.get("/verify/:tx_ref", VerifyPayment);
 // get all payments
-paymentRouter.get("/", getAllPayments);
+paymentRouter.get("/payments", getAllPayments);
 // get payment by order id
-paymentRouter.get("/payments/:orderId", getPaymentByOrderId);
+paymentRouter.get("/updatePayment/:orderId", getPaymentByOrderId);
+
+
 export default paymentRouter;
