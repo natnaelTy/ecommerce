c0a0136fa1d58995870e26f169429a894047a1cc
Natnael taye <natitaye316@gmail.com>
2025-09-28 22:06:12 +0300

    feat: enhance Notifications component with loading state and improved styling

--- frontend/src/components/pages/account/Notifications.jsx
+++ frontend/src/components/pages/account/Notifications.jsx
@@ -1,6 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchNotifications, markNotificationAsRead } from "../../../store/user/userSlice";
+import Loading from "../../../utils/loading/Loading";
+
+
 
 export default function Notifications() {
   const dispatch = useDispatch();
@@ -12,19 +15,23 @@ export default function Notifications() {
     }
   }, [user, dispatch]);
 
+  if(loading)
+    return <Loading />;
+
   return (
-    <div>
+    <div className="max-w-5xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Notifications</h1>
-      {loading && <p>Loading...</p>}
       {error && <p className="text-red-500">{error}</p>}
       {notifications && notifications.length > 0 ? (
         <ul>
           {notifications.map((note) => (
-            <li key={note.id} className={`mb-4 p-3 border rounded ${note.read ? "bg-gray-100" : "bg-white"}`}>
+            <li key={note.id} className={`mb-4 p-3 border border-gray-300 rounded-md ${note.isRead ? "bg-gray-100" : "bg-white border-1 border-pink-500"}`}>
               <div className="font-semibold">{note.title}</div>
               <div>{note.message}</div>
-              <div className="text-xs text-gray-400">{new Date(note.date).toLocaleString()}</div>
-              {!note.read && (
+              <div className="text-xs text-gray-400">{new Date(note.createdAt).toLocaleString("en-US",
+                { dateStyle: "medium", timeStyle: "short" }
+              )}</div>
+              {!note.isRead && (
                 <button
                   className="text-blue-500 text-xs mt-1"
                   onClick={() => dispatch(markNotificationAsRead(note.id))}
