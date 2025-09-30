240f4bc624b8cc78a25d3f2fe3616883ad9c3272
Natnael taye <natitaye316@gmail.com>
2025-09-30 22:07:20 +0300

    fix: update AdminRoute to handle admin role validation and error notifications

--- frontend/src/Layout/AdminRoute.jsx
+++ frontend/src/Layout/AdminRoute.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Navigate } from "react-router-dom";
 import SideBar from "../components/pages/adminSide/adminlayout/SideBar";
 import Navbar from "../components/pages/adminSide/adminlayout/Navbar";
 import { getAdminProfile } from "../store/adminside/adminAuthSlice";
-import Loading from "../utils/loading/Loading";
+import toast from "react-hot-toast";
+
+
 
 export default function AdminRoute({ children }) {
-  const { admin, loading } = useSelector((state) => state.adminAuth);
+  const { admin, error } = useSelector((state) => state.adminAuth);
   const dispatch = useDispatch();
   const adminChildren = React.Children.toArray(children);
 
-  useEffect(() => {
-    dispatch(getAdminProfile());
-  }, [dispatch]);
-
-
-  if (admin?.role !== "ADMIN") {
-    return <Navigate to="/admin-login" replace />;
-  }
-
-  return (
-    <div className="flex h-screen bg-gray-50">
-      <SideBar />
-      <div className="flex flex-col flex-1">
-        <Navbar />
-        <main className="flex-1 overflow-y-auto p-4">{adminChildren}</main>
+    useEffect(() => {
+      dispatch(getAdminProfile());
+    }, [dispatch]);
+    
+  if (admin && admin?.role === "ADMIN") {
+    return (
+      <div className="flex h-screen bg-gray-50">
+        <SideBar />
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          <main className="flex-1 overflow-y-auto p-4">{adminChildren}</main>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (error) {
+    return toast.error(error);
+  }
 }
