f91586cc3e05fc51f138a21085a29aeb15021224
Natnael taye <natitaye316@gmail.com>
2025-10-02 13:05:01 +0300

    fix: update CORS configuration to allow requests from the production frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(passport.initialize());
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://myfrontend.vercel.app", credentials: true}));
 
 
 app.use(express.json());
