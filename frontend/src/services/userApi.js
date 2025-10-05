9f0974a278e8809c0fb7d30c7abb8fa83255b73b
Natnael taye <natitaye316@gmail.com>
2025-10-05 12:25:17 +0300

    fix: move withCredentials option to defaults for user API instance

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// Axios instance
-const userApi = axios.create({
-  baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
-  withCredentials: true,
-});
+const userApi = axios.create({ baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth" });
 
+userApi.defaults.withCredentials = true;
 
 export default userApi;
