b2e5fa1ca938889344a244cd3258bd06e7d92e28
Natnael taye <natitaye316@gmail.com>
2025-10-13 18:32:37 +0300

    fix: update cookie settings in generateTokenSetCookie for environment-specific behavior

--- backend/utils/generateTokenSetCookie.js
+++ backend/utils/generateTokenSetCookie.js
@@ -13,10 +13,10 @@ export const generateTokenSetCookie = (res, userId, role) => {
     expiresIn: "7d",
   });
 
-  res.cookie("token", token, {
+    res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
