19e6f14a82dff17bb7788c3de618cf8a58cea7c3
Natnael taye <natitaye316@gmail.com>
2025-10-14 21:27:28 +0300

    fix: remove mobile token handling from userApi interceptor

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -3,17 +3,9 @@ import axios from "axios";
 const userApi = axios.create({
   baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
   withCredentials: true,
-});
-
-const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-
-userApi.interceptors.request.use((config) => {
-  if (isMobile) {
-    const { default: store } = require("../store/index.js");
-    const token = store.getState().user.token;
-    if (token) config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
+    headers: {
+    "Content-Type": "application/json",
+    },
 });
 
 export default userApi;
