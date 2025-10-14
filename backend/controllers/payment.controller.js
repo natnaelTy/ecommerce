c02515b54cf1ff1fcfa25e2ff05260edd6a79bef
Natnael taye <natitaye316@gmail.com>
2025-10-14 19:09:08 +0300

    feat: add getPaymentsByUserId method to retrieve payments for a specific user

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -161,3 +161,26 @@ export const getPaymentByOrderId = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+// get payments by user id
+export const getPaymentsByUserId = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const payments = await prisma.payment.findMany({
+      where: { userId: parseInt(userId) },
+      include: {
+        order: {
+          include: {
+            user: true,
+            orderItems: { include: { product: true } },
+          },
+        },
+      },
+    });
+    res.json(payments);
+  } catch (error) {
+    console.error("Error fetching payments:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
