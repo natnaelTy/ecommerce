ec9c2a0897078642ee8ce757e34cc28be31a782e
Natnael taye <natitaye316@gmail.com>
2025-10-14 21:03:33 +0300

    fix: update token retrieval method in userApi for mobile devices

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,17 +1,15 @@
 import axios from "axios";
-import { store }from "../store";
 
 const userApi = axios.create({
   baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
   withCredentials: true,
 });
 
-// helper to detect mobile devices
 const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
 
-// attach token for mobile only
 userApi.interceptors.request.use((config) => {
   if (isMobile) {
+    const { default: store } = require("../store/index.js");
     const token = store.getState().user.token;
     if (token) config.headers["Authorization"] = `Bearer ${token}`;
   }
