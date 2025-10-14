fb79b335f094f879568d94a89ea78c8aa2cc3f95
Natnael taye <natitaye316@gmail.com>
2025-10-14 22:26:12 +0300

    refactor: remove Content-Type header and restructure userApi interceptor for cleaner request handling

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -3,9 +3,14 @@ import axios from "axios";
 const userApi = axios.create({
   baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
   withCredentials: true,
-    headers: {
-    "Content-Type": "application/json",
-    },
+});
+
+userApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default userApi;
