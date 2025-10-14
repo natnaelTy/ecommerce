9fde703c8463d485f03cfb204ef36d0c2bdba264
Natnael taye <natitaye316@gmail.com>
2025-10-14 20:29:43 +0300

    fix: correct token retrieval from store in userApi interceptor

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { store } from "../redux/store";
+import store from "../store";
 
 const userApi = axios.create({
   baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
@@ -12,7 +12,7 @@ const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
 // attach token for mobile only
 userApi.interceptors.request.use((config) => {
   if (isMobile) {
-    const token = store.getState().auth.token;
+    const token = store.getState().user.token;
     if (token) config.headers["Authorization"] = `Bearer ${token}`;
   }
   return config;
