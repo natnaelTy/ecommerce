830b475a2e81a680845025d582913d269e8ab76f
Natnael taye <natitaye316@gmail.com>
2025-10-15 13:53:17 +0300

    refactor: remove mobile browser detection and token handling from userApi

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// helper to detect mobile/in-app browsers
-const isMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
-
 // create axios instance
 const userApi = axios.create({
   baseURL: "https://ecommerce-ib95q.sevalla.app/api/auth",
@@ -10,13 +7,5 @@ const userApi = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-// attach token header if on mobile 
-userApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem("authToken");
-  if (isMobile && token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
 
 export default userApi;
